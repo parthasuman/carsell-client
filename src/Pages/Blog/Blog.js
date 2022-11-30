8c2821385d2d6df5abba805160e033768b6e4eb4
parthasuman <parthasuman123@gmail.com>
2022-11-30 16:31:41 +0600

    blog question add

--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -1,7 +1,17 @@
 import React from "react";
 
 const Blog = () => {
-  return <div>This is Blog Pages.</div>;
+  return (
+    <div>
+      <h2 className="text-4xl my-4">This is Blog Pages.</h2>
+      <h4 className="text-2xl my-4">
+        What are the different ways to manage a state in a React application?
+      </h4>
+      <h4 className="text-2xl my-4"></h4>
+      <h4 className="text-2xl my-4"></h4>
+      <h4 className="text-2xl my-4"></h4>
+    </div>
+  );
 };
 
 export default Blog;
